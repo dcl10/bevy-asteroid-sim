63fac96e0d3b92737ca45a0cc0ab0b2e638d59d9
Daniel Lea <dan.carr.lea@gmail.com>
2023-10-24 21:08:30 +0100

    Feature/asteroids (#3)
    * `AsteroidSpawnTimer`
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * tick asteroid timer
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * `Asteroid` component
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * asteroid spawning
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    ---------
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use bevy::{prelude::*};
 
-use crate::systems::{spawn_camera, spawn_planet};
+use crate::resources::AsteroidSpawnTimer;
+use crate::systems::{spawn_asteroid, spawn_camera, spawn_planet, tick_asteroid_spawn_timer};
 
 mod components;
 mod systems;
+mod resources;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_camera, spawn_planet))
+        .init_resource::<AsteroidSpawnTimer>()
+        .add_systems(Startup, (spawn_camera, spawn_planet).chain())
+        .add_systems(Update, (tick_asteroid_spawn_timer, spawn_asteroid))
         .run();
 }
