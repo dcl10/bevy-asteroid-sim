591e9cc0bde8bbe292793cae881e3cdd193af3bf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-10-24 20:01:06 +0100

    `AsteroidSpawnTimer`
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::{prelude::*};
 
+use crate::resources::AsteroidSpawnTimer;
 use crate::systems::{spawn_camera, spawn_planet};
 
 mod components;
 mod systems;
+mod resources;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_resource::<AsteroidSpawnTimer>()
         .add_systems(Startup, (spawn_camera, spawn_planet))
         .run();
 }
---
+++ src/resources.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+const ASTEROID_SPAWN_DELAY_SECS: f32 = 5.0;
+
+#[derive(Resource)]
+pub struct AsteroidSpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for AsteroidSpawnTimer {
+    fn default() -> AsteroidSpawnTimer {
+        AsteroidSpawnTimer { timer: Timer::from_seconds(ASTEROID_SPAWN_DELAY_SECS, TimerMode::Repeating) }
+    }
+}
