a0f12638534dc61ec091c6b50e846d6aa750ae70
Daniel Lea <dan.carr.lea@gmail.com>
2023-10-25 19:07:33 +0100

    Feature/collision (#5)
    * collision of asteroids with planet
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * install itertools
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    * asteroid collision
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>
    
    ---------
    
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::{prelude::*};
 
 use crate::resources::AsteroidSpawnTimer;
-use crate::systems::{move_asteroids, spawn_asteroid, spawn_camera, spawn_planet, tick_asteroid_spawn_timer};
+use crate::systems::{collide_asteroids, collide_asteroids_with_planet, move_asteroids, spawn_asteroid, spawn_camera, spawn_planet, tick_asteroid_spawn_timer};
 
 mod components;
 mod systems;
@@ -14,5 +14,7 @@ fn main() {
         .add_systems(Startup, (spawn_camera, spawn_planet).chain())
         .add_systems(Update, (tick_asteroid_spawn_timer, spawn_asteroid))
         .add_systems(Update, move_asteroids)
+        .add_systems(Update, collide_asteroids_with_planet)
+        .add_systems(Update, collide_asteroids)
         .run();
 }
