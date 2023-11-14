5fa12cc8bcca4b6a9d2b6b81ea91123d061bb8a5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-11-14 17:19:42 +0000

    test between
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- src/traits.rs
+++ src/traits.rs
@@ -12,7 +12,31 @@ where
 }
 
 impl Between<f32> for f32 {
+    /// Returns `true` if `lower` < `self` < `upper`, else false.
+    ///
+    /// # Arguments
+    /// * `lower` - the lower bound
+    /// * `upper` - the upper bound
+    ///
+    /// # Examples
+    /// ```rust
+    /// assert!(42.0.between(41.0, 43.0));
+    /// assert!(!42.0.between(43.0, 44.0));
+    /// assert!(!42.0.between(43.0, 41.0));
+    /// ```
     fn between(&self, lower: f32, upper: f32) -> bool {
         lower.lt(self) && upper.gt(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_between_f32() {
+        assert!(42.0.between(41.0, 43.0));
+        assert!(!42.0.between(43.0, 44.0));
+        assert!(!42.0.between(43.0, 41.0))
     }
 }
